7bce2a6643d9074fea7e78b451b4905ece1c775a
Dipika Sharma <officialdipikasharma@gmail.com>
2026-07-16 15:41:47 -0400

    Revert "Entire"
    This reverts commit 378c4a99130c08d4648da7f215c4b3b9218e48e0.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import MenteeActivityDashboard from './components/MenteeActivityDashboard'
-import LearningPath from './components/LearningPath'
-import CCSSurvey from './components/CCSSurvey'
-import SurveyResults from './components/SurveyResults'
 import LearningHub from './components/LearningHub'
 import CareerPathway from './components/CareerPathway'
 import NavRail from './components/NavRail'
@@ -16,12 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<CtfHero />} />
           <Route path="/dashboard" element={<MenteeActivityDashboard />} />
-          <Route path="/learn" element={<LearningPath />} />
-          <Route path="/survey" element={<CCSSurvey />} />
-          <Route path="/survey-results" element={<SurveyResults />} />
-          <Route path="/learn" element={<LearningPath />} />
-          <Route path="/survey" element={<CCSSurvey />} />
-          <Route path="/survey-results" element={<SurveyResults />} />
           <Route path="/hub" element={<LearningHub />} />
           <Route path="/pathway" element={<CareerPathway />} />
         </Routes>
